8a2f97494fe9cbb7bac00e8820a44f95cbf1db4f
KNSEntertainment <entertainmentkns@gmail.com>
2026-01-11 12:50:32 +0100

    menuItems source changed

--- app/[locale]/dashboard/layout.jsx
+++ app/[locale]/dashboard/layout.jsx
@@ -3,15 +3,26 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
-import { menuItems } from "@/components/DashboardMenuItems.js";
 import { useSession } from "next-auth/react";
 import { ActiveMenuProvider, useActiveMenu } from "@/context/ActiveMenuContext";
+import { BookImage, Settings, GalleryThumbnails, LayoutDashboard, Book, Newspaper, User, Download } from "lucide-react";
 
 function DashboardLayoutContent({ children }) {
 	const { activeMenu } = useActiveMenu();
 	const router = useRouter();
 	const [profileOpen, setProfileOpen] = useState(false);
 	const { data: session, status } = useSession();
+
+	const menuItems = [
+		{ id: "dashboard", label: "Dashboard", icon: LayoutDashboard, color: "bg-brand", href: "/en/dashboard" },
+		{ id: "contactmessages", label: "Contact Messages", icon: Book, color: "bg-red-900", href: "/en/dashboard/contactmessages" },
+		{ id: "events", label: "Events", icon: BookImage, color: "bg-purple-500", href: "/en/dashboard/events" },
+		{ id: "blogs", label: "Blogs", icon: Newspaper, color: "bg-orange-700", href: "/en/dashboard/blogs" },
+		{ id: "gallery", label: "Gallery", icon: GalleryThumbnails, color: "bg-orange-500", href: "/en/dashboard/gallery" },
+		{ id: "downloads", label: "Downloads", icon: Download, color: "bg-red-500", href: "/en/dashboard/downloads" },
+		{ id: "users", label: "Users", icon: User, color: "bg-green-700", href: "/en/dashboard/users" },
+		{ id: "settings", label: "Profile Settings", icon: Settings, color: "bg-gray-500", href: "/en/dashboard/settings" },
+	];
 
 	// Protect dashboard: redirect if not authenticated
 	if (status === "loading") {
